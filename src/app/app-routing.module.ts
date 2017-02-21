e98f4890eb1615f8bea2382b152d08a678c693c3
saabirmohamed <saabirgmohamed@gmail.com>
2017-02-21 16:17:05 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', loadChildren: './modules/home/home.module#HomeModule' },
+  { path: 'home', loadChildren: './modules/home/home.module#HomeModule' },
+  { path: 'services', loadChildren: './modules/services/services.module#ServicesModule' },
+  { path: 'contact', loadChildren: './modules/contact/contact.module#ContactModule' },
+  { path: 'about', loadChildren: './modules/about/about.module#AboutModule' },
+  { path: 'login', loadChildren: './modules/login/login.module#LoginModule' }
 ];
 
 @NgModule({
